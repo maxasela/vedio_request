81d99f258a55adfa00cf62e9c2b1a328d644e069
maxasela <maxasela@gmail.com>
2016-03-06 22:01:58 +0530

    First commit

--- index.php
+++ index.php
@@ -3,16 +3,25 @@
 <head>
 <title>Request Vedio</title>
 
-<script type="text/javascript">
-  
-  $(document).ready(function(){
-    $('.collapsible').collapsible({
-      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
-  });
-       
 
-</script>
+
+<!-- -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript">  
+        $(document).ready(function(){
+            $("#report tr:odd").addClass("odd");
+            $("#report tr:not(.odd)").hide();
+            $("#report tr:first-child").show();
+            
+            $("#report tr.odd").click(function(){
+                $(this).next("tr").toggle();
+                $(this).find(".arrow").toggleClass("up");
+            });
+            //$("#report").jExpand();
+        });
+    </script>
+<!-- -->
+
       <!--Import Google Icon Font-->
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
@@ -26,9 +35,14 @@
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
 
+<center>
 
-
-<table class="highlight responsive-table" style="width:700px; margin:auto;margin-top:100px">
+<h1 style="margin-top:100px;color:#6666ff">  
+Live vedio Request
+</h1>
+<p class="flow-text">Share Knowledge and experience with Others</p>
+</center>
+<table id="report" class="highlight responsive-table" style="width:700px; margin:auto;margin-top:100px">
         <thead>
           <tr>
               <th data-field="id"> </th>
@@ -40,30 +54,64 @@
 
 
         <tbody>
+
           <tr >
             <td>Free</td>
-            <td style="color:blue;height:25px;overflow:hidden;white-space: nowrap;text-overflow: ellipsis;" >Help Making Apple Pie</td>
+            <td style="color:#3232ff;">Help Making Apple Pie
+            
+
+            </td>
             <td>12.00 Today</td>
             <td>Entertainment</td>
           </tr>
+
+           <tr>
+            <td colspan="4">
+                
+                <p>Usually I am good with DIY repairs but i cant figure out how to 
+                get beyond this final piece.I need someone to 
+                take a look ath my car and walk me through this</p>
+                    
+            </td>
+        </tr>
           <tr>
             <td>$25</td>
-            <td style="color:blue;">Need Someone to show me how to fix brakes</td>
+            <td style="color:#3232ff;">Need Someone to show me how to fix brakes</td>
             <td>3.00 3/10</td>
             <td>Expert advice</td>
           </tr>
           <tr>
+            <td colspan="4">
+                
+                <p>Usually I am good with DIY repairs but i cant figure out how to 
+                get beyond this final piece.I need someone to 
+                take a look ath my car and walk me through this</p>
+                    
+            </td>
+        </tr>
+          <tr>
             <td>$20</td>
-            <td style="color:blue;">Democratic National Debate in Montana</td>
+            <td style="color:#3232ff;">Democratic National Debate in Montana</td>
             <td>Anytime</td>
             <td>Event</td>
           </tr>
+
+          <tr>
+            <td colspan="4">
+                
+                <p>Usually I am good with DIY repairs but i cant figure out how to 
+                get beyond this final piece.I need someone to 
+                take a look ath my car and walk me through this</p>
+                    
+            </td>
+        </tr>
 
 
 
         </tbody>
 
       </table>
+
 
 
 
